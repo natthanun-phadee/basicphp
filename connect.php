ae6cf787c063d6b3324b6951d43ba3975b5080e3
jeerayutXD <jeerayutXD@gmail.com>
2024-07-15 15:57:11 +0700

    backupt

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'db_basicphp';
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+if($conn->connect_error) {
+    die('Connect failed: '.$conn->connect_error);
+}
+    
---
+++ while.php
@@ -0,0 +1,9 @@
+<?php
+$i = 1;
+while ($i <= 10) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo $i . "";
+    }
+    echo "<br>";
+    $i++;
+}
